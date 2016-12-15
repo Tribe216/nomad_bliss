b11df3024fad861e1642836a5bf6095b9c11c555
tribe216 <bart.mach@gmail.com>
2016-12-14 19:19:19 -0500

    refactor detail into components

--- frontend/components/detail/detail.jsx
+++ frontend/components/detail/detail.jsx
@@ -1,70 +1,14 @@
 import React from 'react';
-import { barData, metricLongNames } from '../../util/helpers';
+
 import DetailHeader from './detail_header';
+import DetailScoreBarSection from './detail_score_bar_section';
 
 class Detail extends React.Component {
   constructor(props) {
     super(props);
-    this.barData = barData;
-    this.metricLongNames = metricLongNames;
-    this.getScoreBars = this.getScoreBars.bind(this);
-    this.scoreBar = this.scoreBar.bind(this);
-    this.costToScore = this.costToScore.bind(this);
   }
-
-  scoreBar(score) {
-    const baseScore = Math.ceil(score);
-
-    return   (
-      <span className='detail-bar-bg'>
-        <span className='detail-bar-colored'
-          style={{
-            width: this.barData[baseScore].width + '%',
-            backgroundColor: this.barData[baseScore].color,
-            color: this.barData[baseScore].color
-          }}>
-          <span className='detail-color-bar-label'>{ this.barData[baseScore].label }</span>
-        </span>
-      </span>
-    );
-  }
-
-  costToScore(costOfLiving) {
-    return Math.ceil((3500 - costOfLiving) / 320);
-  }
-
-  getScoreBars() {
-    const scores = this.props.cityDetails.scores;
-
-    const bars = [];
-    let val = null;
-    let longName = null;
-
-    Object.keys(scores).forEach( (key, index) => {
-
-      longName = this.metricLongNames[key];
-
-      if (key === 'cost_of_living') {
-        val = this.scoreBar(this.costToScore(scores[key]));
-      } else {
-        val = this.scoreBar(scores[key]);
-      }
-
-      bars.push(
-        <div className='detail-chart-element'>
-          <span className='detail-chart-label'>{ longName }</span>
-            { val }
-        </div>
-      );
-    });
-
-    return bars;
-  }
-
-
 
   render() {
-
     return (
       <section className='detail-modal'>
         <DetailHeader
@@ -72,9 +16,7 @@ class Detail extends React.Component {
           regionName={this.props.cityDetails.region_name}
           imageUrl={this.props.cityDetails.image_url}
         />
-        <section className='detail-chart'>
-          {this.getScoreBars()}
-        </section>
+        <DetailScoreBarSection scores={this.props.cityDetails.scores} />
       </section>
     );
 
---
+++ frontend/components/detail/detail_score_bar_section.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import DetailScoreBar from './detail_score_bar';
+import { metricLongNames } from '../../util/helpers';
+
+class DetailScoreBarSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.metricLongNames = metricLongNames;
+  }
+
+  costToScore(costOfLiving) {
+    return Math.ceil((3500 - costOfLiving) / 320);
+  }
+
+  render() {
+    const scores = this.props.scores;
+
+    const bars = [];
+    let bar = null;
+    let longName = null;
+
+    Object.keys(scores).forEach( (key, index) => {
+
+      longName = this.metricLongNames[key];
+
+      if (key === 'cost_of_living') {
+        bar = <DetailScoreBar score={this.costToScore(scores[key])} />;
+      } else {
+        bar = <DetailScoreBar score={scores[key]} />;
+      }
+
+      bars.push(
+        <div className='detail-chart-element'>
+          <span className='detail-chart-label'>{ longName }</span>
+            { bar }
+        </div>
+      );
+    });
+
+    return (
+      <section className='detail-chart'>
+        { bars }
+      </section>
+    );
+  }
+}
+
+export default DetailScoreBarSection;
