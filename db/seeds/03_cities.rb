33c191c67ef262d4bef0a390982001995aafd3f3
Bart Mach <bart.mach@gmail.com>
2016-12-16 04:12:14 -0500

    added images

--- db/seeds/03_cities.rb
+++ db/seeds/03_cities.rb
@@ -1,130 +1,47 @@
 def get_cities
-   [
- {
-   city_name: "New York",
-   latitude: 40.730610,
-   longitude: -73.935242,
-   region_id: 2,
- },
+  [
 
- {
-   city_name: "Philadelphia",
-   latitude: 39.952583,
-   longitude: -75.165222,
-   region_id: 3,
- },
 
- {
-   city_name: "King of Prussia",
-   latitude: 40.104794,
-   longitude: -75.384392,
-   region_id: 3,
- },
 
- {
-   city_name: "Lansdale",
-   latitude: 40.240159,
-   longitude: -75.275902,
-   region_id: 3,
- },
 
- {
-   city_name: "Allentown",
-   latitude: 40.611279,
-   longitude: -75.483269,
-   region_id: 3,
- },
 
- {
-   city_name: "Newark",
-   latitude: 40.735657,
-   longitude: -74.172363,
-   region_id: 1,
- },
 
- {
-   city_name: "Yonkers",
-   latitude:  40.94139,
-   longitude: -73.86444,
-   region_id: 2,
- },
 
- {
-   city_name: "Farmingdale",
-   latitude:  40.732143,
-   longitude: -73.457120,
-   region_id: 2,
- },
 
- {
-   city_name: "Poughkeepsie",
-   latitude:  41.723526,
-   longitude: -73.928839,
-   region_id: 2
- },
+{ city_name: "Trenton",            latitude: 40.2174053,  longitude: -74.742938,  region_id: 1 },
+{ city_name: "Toms River",         latitude: 39.951141,  longitude: -74.1749498,  region_id: 1 },
+{ city_name: "Atlantic City",      latitude: 39.3594971,  longitude: -74.426024,  region_id: 1 },
+{ city_name: "Ocean City",         latitude: 39.2747443,  longitude: -74.57264405,  region_id: 1 },
+{ city_name: "Newark",             latitude: 40.7356457,  longitude: -74.172363,  region_id: 1, },
+{ city_name: "Cherry Hill",             latitude: 40.7356457,  longitude: -74.41234363,  region_id: 1, },
+{ city_name: "Jersey City",             latitude: 40.7435657,  longitude: -74.1477663,  region_id: 1, },
+{ city_name: "Paterson",             latitude: 40.73455657,  longitude: -74.1776363,  region_id: 1, },
+{ city_name: "New York",           latitude: 40.7304567610,  longitude: -73.123242,  region_id: 2, },
+{ city_name: "Yonkers",            latitude:  40.93454139,  longitude: -73.123444,   region_id: 2, },
+{ city_name: "Farmingdale",        latitude:  40.7321234143, longitude: -73.1237120,  region_id: 2, },
+{ city_name: "Poughkeepsie",       latitude:  41.724573526, longitude: -73.1238839,  region_id: 2 },
+{ city_name: "Buffalo",           latitude:  41.7231324526, longitude: -73.123839,  region_id: 2 },
+{ city_name: "Rochester",         latitude:  41.724573526, longitude: -73.9128839,  region_id: 2 },
+{ city_name: "Syracuse",          latitude:  41.723457457526, longitude: -73.91345839,  region_id: 2 },
+{ city_name: "Albany",            latitude:  41.723526, longitude: -73.9435839,  region_id: 2 },
+{ city_name: "New Rochelle",       latitude:  41.7245673526, longitude: -73.985339,  region_id: 2 },
+{ city_name: "Schenectady",       latitude:  41.724573526, longitude: -73.9387839,  region_id: 2 },
+{ city_name: "Philadelphia",       latitude: 39.954672583,  longitude: -75.11452322,  region_id: 3, },
+{ city_name: "King of Prussia",    latitude: 40.104574794,  longitude: -75.3124567392,  region_id: 3, },
+{ city_name: "Lansdale",           latitude: 40.244574570159,  longitude: -75.212302,  region_id: 3, },
+{ city_name: "Allentown",          latitude: 40.611279,  longitude: -75.483269,  region_id: 3, },
+{ city_name: "Scranton",           latitude: 41.404578970,  longitude: -75.662415,  region_id: 3 },
+{ city_name: "Pittsburgh",           latitude: 41.40857970,  longitude: -75.6125763415,  region_id: 3 },
+{ city_name: "Reading",           latitude: 41.408457970,  longitude: -75.6123445715,  region_id: 3 },
+{ city_name: "Altoona",           latitude: 41.4089456770,  longitude: -75.6612745715,  region_id: 3 },
+{ city_name: "Williamsport",           latitude: 41.445708970,  longitude: -75.66123415,  region_id: 3 },
+{ city_name: "Montreal",           latitude: 45.45675,       longitude: -73.5612367,  region_id: 4, },
+{ city_name: "Quebec City",        latitude: 46.8145739,    longitude: -71.207680,    region_id: 4, },
+{ city_name: "Toronto",            latitude: 43.7645671539,  longitude: -79.4141079,  region_id: 5, },
+{ city_name: "Ottawa",             latitude: 45.425447533,  longitude: -75.6924457482,  region_id: 5, }
 
- {
-   city_name: "Trenton",
-   latitude: 40.217053,
-   longitude: -74.742938,
-   region_id: 1
- },
 
- {
-   city_name: "Toms River",
-   latitude: 39.951141,
-   longitude: -74.179498,
-   region_id: 1
- },
 
- {
-   city_name: "Atlantic City",
-   latitude: 39.359971,
-   longitude: -74.426024,
-   region_id: 1
- },
-
- {
-   city_name: "Ocean City",
-   latitude: 39.277443,
-   longitude: -74.572605,
-   region_id: 1
- },
-
- {
-   city_name: "Scranton",
-   latitude: 41.408970,
-   longitude: -75.662415,
-   region_id: 3
- },
-
- {
-   city_name: "Toronto",
-   latitude: 43.761539,
-   longitude: -79.411079,
-   region_id: 5,
- },
-
- {
-   city_name: "Ottawa",
-   latitude: 45.425533,
-   longitude: -75.692482,
-   region_id: 5,
- },
-
- {
-   city_name: "Montreal",
-   latitude: 45.5,
-   longitude: -73.566667,
-   region_id: 4,
- },
-
- {
-   city_name: "Quebec City",
-   latitude: 46.8139,
-   longitude: -71.2080,
-   region_id: 4,
- }
-]
+  ]
 
 end
